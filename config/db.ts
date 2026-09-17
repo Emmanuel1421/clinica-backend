eddc628b8c43b0ea3d33e4f35451697eb5163ea5
Emmanuel1421 <emmanuelguerra1945@gmail.com>
2026-09-17 19:30:19 -0300

    criado o tsconfig e alteração na constante do dbPath

--- config/db.ts
+++ config/db.ts
@@ -1,10 +1,12 @@
 import Database from 'better-sqlite3';
+import type { Database as SqliteDatabase } from 'better-sqlite3';
 import path from 'path';
 
 // Define o caminho do banco de dados (pode vir de variável de ambiente, ou padrão para database.sqlite)
 const dbPath = process.env.DATABASE_URL || path.resolve(__dirname, '../../database.sqlite');
 
-export const db = new Database(dbPath, { verbose: console.log });
+export const db: SqliteDatabase = new Database(dbPath, { verbose: console.log });
+
 
 // Habilita chaves estrangeiras no SQLite
 db.pragma('foreign_keys = ON');
